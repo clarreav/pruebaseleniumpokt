0714272fb35332556fb33cb3329606bdf326a04b
clarreav <73416935+clarreav@users.noreply.github.com>
2022-04-03 14:06:52 -0500

    Update sele.py

--- sele.py
+++ sele.py
@@ -13,10 +13,22 @@ driver.get("https://thunderpokt.fi/analytics")
 time.sleep(8)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 data = soup.find('body', class_='h-full').find_all('h4', class_='text-xl font-semibold')
-apy, apyn = data[0].text.split(" ")
-dr, drn = data[1].text.split(" ")
-tts, ttsn = data[2].text.split(" ")
-_1d, _1dn = data[4].text.split(" ")
-_3d, _3dn = data[5].text.split(" ")
-_7d, _7dn = data[6].text.split(" ")
-print(apyn, apy)
+apy = data[0].text.split(" ")
+apy_number = apy[0]
+apy_name = apy[2]
+dr = data[1].text.split(" ")
+dr_number = dr[0]
+dr_name = dr[2]
+tts = data[2].text.split(" ")
+tts_number = tts[0]
+tts_name = tts[2]
+_1d = data[4].text.split(" ")
+_1d_number = _1d[0]
+_1d_name = _1d[2]
+_3d = data[5].text.split(" ")
+_3d_number = _3d[0]
+_3d_name = _3d[2]
+_7d = data[6].text.split(" ")
+_7d_number = _7d[0]
+_7d_name = _7d[2]
+print(apy_name, apy_number)

8fb23331d7ba0543a3eff64b0cca95c01810bb1b
clarreav <73416935+clarreav@users.noreply.github.com>
2022-04-03 13:51:02 -0500

    Update sele.py

--- sele.py
+++ sele.py
@@ -2,14 +2,13 @@ from selenium import webdriver
 import os
 import time
 from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.service import Service
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 driver.get("https://thunderpokt.fi/analytics")
 time.sleep(8)
 soup = BeautifulSoup(driver.page_source, 'html.parser')

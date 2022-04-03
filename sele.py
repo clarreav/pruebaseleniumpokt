980a0aafab4f71d8e85bfcc9476f0aa7abc3228a
clarreav <73416935+clarreav@users.noreply.github.com>
2022-04-03 13:59:54 -0500

    Update sele.py

--- sele.py
+++ sele.py
@@ -13,10 +13,10 @@ driver.get("https://thunderpokt.fi/analytics")
 time.sleep(8)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 data = soup.find('body', class_='h-full').find_all('h4', class_='text-xl font-semibold')
-apy = data[0].text
-dr = data[1].text
-tts = data[2].text
-_1d = data[4].text
-_3d = data[5].text
-_7d = data[6].text
-print(apy, dr, tts, _1d, _3d, _7d)
+apy, apyn = data[0].text.split(" ")
+dr, drn = data[1].text.split(" ")
+tts, ttsn = data[2].text.split(" ")
+_1d, _1dn = data[4].text.split(" ")
+_3d, _3dn = data[5].text.split(" ")
+_7d, _7dn = data[6].text.split(" ")
+print(apyn, apy)
